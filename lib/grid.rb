996d7e7ca337ee0ecb2fd8148161259a4028a160
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-02-03 18:50:47 -0500

    Create intial grid-generation logic
    Ideally, to make user selection easier later on, the grid is composed of
    the numbers 1 through 9 arranged in a 3×3 layout.
    
    This is implemented using a nested array, [[1,2,3],[4,5,6],[7,8,9]],
    along with nested .each loops. The inner loop prints each individual
    item, while the outer loop inserts a new line after each row is
    displayed.

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,5 +1,16 @@
-class Grid 
+class Grid
 
-  
-  
+  def generate_grid()
+
+    nested_array = [[1,2,3],[4,5,6],[7,8,9]]
+
+    nested_array.each do |item|
+      item.each do |inn_item|
+        print "#{inn_item} "
+      end
+      print "\n"
+    end
+
+  end 
+   
 end
