178fcb021be06edcdc723ac70714196835f855e4
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-02-12 19:54:42 -0500

    Refine modify_grid method
    The method now also accepts an input argument. This allows the Player
    class to pass in an 'X' and the Computer class to pass in an 'O' when
    selecting a position on the grid.

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,6 +1,6 @@
 class Grid
 
-  attr_accessor :nested_array
+  attr_reader :nested_array
 
   def initialize
     @nested_array = [[1,2,3],[4,5,6],[7,8,9]]
@@ -19,12 +19,12 @@ class Grid
 
   end
 
-  def modify_grid(position)
+  def modify_grid(position, input)
 
     nested_array.each.with_index do |row, r_i|
       row.each.with_index do |value, v_i|
         if value == position
-          nested_array[r_i][v_i] = 'X'
+          nested_array[r_i][v_i] = input
         end
       end
     end
