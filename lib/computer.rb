eeceb94a075e90eeda8f3bb73506fd99c0f4032d
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-02-15 15:52:41 -0500

    Correct infinite loop in computer_choice
    An infinite loop was occurring in the main game loop. The issue was
    caused by the while loop that repeatedly checked for a new position
    whenever the randomly selected space was already taken.
    
    To resolve this, an additional condition was added to the while loop
    inside computer_choice. This condition verifies whether all positions
    in the grid have already been converted to strings (i.e., all spaces are
    occupied).
    
    A boolean variable (@all_strings) is updated each time a random position
    is selected. If the boolean indicates that all spaces are taken, the
    loop will not execute again, preventing the infinite loop from occurring.

--- lib/computer.rb
+++ lib/computer.rb
@@ -6,7 +6,7 @@ class Computer
     @x = 0
     @y = 0
     @choice = 0
-    @chose_first = false
+    @all_strings = false
     @grid = grid
   end
 
@@ -14,23 +14,23 @@ class Computer
   x = rand(0..2)
   y = rand(0..2)
 
-  @choice = @grid.nested_array[x][y]
+  @choice = @grid.nested_array[x][y]  
+  @all_strings = @grid.nested_array.all? {|row| row.all? {|e| e.is_a?(String)}}
   end
 
   def computer_choice()
-    unless @chose_first
-      choose_random()
 
-      if @choice.is_a?(Numeric)
-        @grid.modify_grid(@choice, 'O')
-      else
-        while @choice.is_a?(String)
-          choose_random()
-          return @grid.modify_grid(@choice, 'O') if @choice.is_a?(Numeric)
-        end
+    choose_random()
+
+    if @choice.is_a?(Numeric)
+      @grid.modify_grid(@choice, 'O')
+    else
+      while @choice.is_a?(String) && (@all_strings == false)
+        choose_random()
+        return @grid.modify_grid(@choice, 'O') if @choice.is_a?(Numeric)
       end
-      
-    end    
+    end
+
   end
 
 end
