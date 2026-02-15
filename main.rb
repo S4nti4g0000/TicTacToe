e798ca401a4ee9731066b71d56cfaf97e650f0dc
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-02-15 16:16:08 -0500

    Create basic game loop
    First, the grid is generated. Then, a boolean variable starts the main
    game loop and determines whose turn it is. The loop continues alternating
    turns until all positions on the grid have been occupied, at which point
    the loop terminates.

--- main.rb
+++ main.rb
@@ -6,4 +6,18 @@ new_grid = Grid.new
 player = Player.new(new_grid)
 computer = Computer.new(new_grid)
 
-computer.computer_choice
+game_on = true
+
+new_grid.generate_grid
+
+while game_on
+
+  puts "- Player's Turn - \n"
+  player.player_choice
+  puts "- Computer's Turn - \n"
+  computer.computer_choice
+
+  break if new_grid.nested_array.all? {|row| row.all? {|e| e.is_a?(String)}}
+
+end
+
