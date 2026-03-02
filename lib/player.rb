ca6ae3e1e8538172625c8a537b1c089ba454c362
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-03-02 18:49:36 -0500

    Include a retry if position is already taken
    The code first verifies whether the player's selection exists within the
    array. If the selection is valid, the array is updated immediately.
    Otherwise, an until loop prompts the user repeatedly until a valid
    option is provided. Once a valid choice is entered, the array is
    modified accordingly and execution continues.
    
    Additionally, a can_continue boolean flag was introduced to manage the
    control flow within the main loop, ensuring proper progression of the
    interaction.

--- lib/player.rb
+++ lib/player.rb
@@ -2,16 +2,34 @@ require_relative 'grid'
 
 class Player
 
-    def initialize(grid)
-      @choice = 0
-      @grid = grid
-    end
-    
+  attr_reader :can_continue
 
-    def player_choice ()
-        print "Choose a position from the board: "
+  def initialize(grid)
+    @choice = 0
+    @can_continue = false
+    @grid = grid
+  end
+  
+  def player_choice ()
+    @can_continue = false
+
+    print "Choose a position from the board: "
+    choice = gets.chomp.to_i
+
+    if @grid.nested_array.flatten.include?(choice)
+      @grid.modify_grid(choice, 'X')
+      @can_continue = true
+    else
+
+      until @grid.nested_array.flatten.include?(choice)
+        print "Wrong or already taken. Please choose another: "
         choice = gets.chomp.to_i
-        @grid.modify_grid(choice, 'X')
-    end
+      end
+
+      @grid.modify_grid(choice, 'X')
+      @can_continue = true
+    end   
+    
+  end
 
 end
